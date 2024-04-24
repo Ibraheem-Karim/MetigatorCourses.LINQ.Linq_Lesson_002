06b69c96ec8dcae719d7d65cc1c324fa21912184
IbraheemKarim <ebrahimabdelkarim57@gmail.com>
2024-04-24 23:57:30 +0200

    Add practice solution

---
+++ Practice/Delegates/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Delegates
+{
+
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action action = m1;
+
+            m2(action);
+        }
+
+
+        static void m1()
+        {
+            Console.WriteLine("m1");
+        }
+        static void m2(Action m1)
+        {
+            m1();
+            Console.WriteLine("m2");
+        }
+
+    }
+}
---
+++ Practice/ProceduralWay/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProceduralWay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
